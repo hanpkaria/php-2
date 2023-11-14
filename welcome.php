604168809e8a81137b647937c74ca5f12b71bef2
hanpkaria <9023367@student.zadkine.nl>
2023-11-14 14:32:18 +0100

    1

---
+++ welcome.php
@@ -0,0 +1,13 @@
+<?php
+
+echo "<h1>Hallo wereld,</h1>";
+echo "Ik zit op het ";
+
+$schoolnaam = "Techniek College Rotterdam !";
+
+
+echo "$schoolnaam";
+$eigenVariabele = "Dit is mijn eigen variabele."
+echo "<p>$eigenVariabele</p>";
+
+?>
